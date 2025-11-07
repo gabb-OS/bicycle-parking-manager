fd187af748c8d897ef54c39ebaca5caa4b8906c0
Gabriele Aprile <gabriele.aprile2@studio.unibo.it>
2025-11-07 16:50:45 +0100

    Added sample layer to map

--- frontend-bpm/src/app/components/map/map.ts
+++ frontend-bpm/src/app/components/map/map.ts
@@ -4,6 +4,11 @@ import View from 'ol/View';
 import TileLayer from 'ol/layer/Tile';
 import OSM from 'ol/source/OSM';
 import { fromLonLat } from 'ol/proj';
+import VectorLayer from 'ol/layer/Vector';
+import VectorSource from 'ol/source/Vector';
+import GeoJSON from 'ol/format/GeoJSON';
+import Style from 'ol/style/Style';
+import Icon from 'ol/style/Icon';
 
 @Component({
   selector: 'app-map',
@@ -12,6 +17,36 @@ import { fromLonLat } from 'ol/proj';
   styleUrl: './map.css',
 })
 export class MapComponent implements OnInit {
+private bicycleParkingData = {
+    'type': 'FeatureCollection',
+    'features': [
+      {
+        'type': 'Feature',
+        'properties': {
+          'name': 'Piazza Maggiore Bike Park',
+          'capacity': 50,
+          'type': 'covered'
+        },
+        'geometry': {
+          'type': 'Point',
+          'coordinates': [11.342977, 44.494139]
+        }
+      },
+      {
+        'type': 'Feature',
+        'properties': {
+          'name': 'Stazione Centrale Bike Park',
+          'capacity': 100,
+          'type': 'covered'
+        },
+        'geometry': {
+          'type': 'Point',
+          'coordinates': [11.341970, 44.506673]
+        }
+      }
+    ]
+  };
+
   private map: Map | undefined;
 
   private view = new View({
@@ -22,13 +57,27 @@ export class MapComponent implements OnInit {
   private osmLayer = new TileLayer({
     source: new OSM({
     })
-});
+  });
+
+  private testGeoJSONLayer = new VectorLayer({
+    source: new VectorSource({
+      features: new GeoJSON().readFeatures(this.bicycleParkingData, {
+        featureProjection: 'EPSG:3857'
+      })
+    }),
+    style: new Style({
+      image: new Icon({
+        src: 'assets/mapIcons/museum_icon.png',
+        scale: 0.05
+      })
+    })
+  });
 
   ngOnInit() {
     this.map = new Map({
       target: 'map',
       view: this.view,
-      layers: [this.osmLayer]
+      layers: [this.osmLayer, this.testGeoJSONLayer]
     });
   }
 }
