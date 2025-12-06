5b104ccbafb4abb18118c799d827a674834c648f
Gabriele Aprile <gabriele.aprile2@studio.unibo.it>
2025-12-06 17:34:37 +0100

    chore: changed temporary json data

--- frontend-bpm/src/app/components/map/map.ts
+++ frontend-bpm/src/app/components/map/map.ts
@@ -23,36 +23,366 @@ import Fill from 'ol/style/Fill';
 })
 export class MapComponent implements OnInit {
 private bicycleParkingData = {
-    'type': 'FeatureCollection',
-    'features': [
-      {
-        'type': 'Feature',
-        'properties': {
-          'name': 'Piazza Maggiore Bike Park',
-          'capacity': 50,
-          'type': 'covered'
-        },
-        'geometry': {
-          'type': 'Polygon',
-          'coordinates': [
-            [[11.342977, 44.494139], [11.341211, 44.497685], [11.340154, 44.494021]]
-          ]
-        }
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "id": 1001,
+        "name": "Filippo Re - Dip. Fisiologia",
+        "maxCapacity": 50,
+        "residualCapacity": 50
       },
-      {
-        'type': 'Feature',
-        'properties': {
-          'name': 'Stazione Centrale Bike Park',
-          'capacity': 100,
-          'type': 'covered'
-        },
-        'geometry': {
-          'type': 'Point',
-          'coordinates': [11.341970, 44.506673]
-        }
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [
+          [
+            [
+              11.355749,
+              44.499046
+            ],
+            [
+              11.355684,
+              44.498491
+            ],
+            [
+              11.355781,
+              44.498503
+            ],
+            [
+              11.355749,
+              44.499046
+            ]
+          ]
+        ]
       }
-    ]
-  };
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "id": 1002,
+        "name": "Irnerio - Dip. Farmacia",
+        "maxCapacity": 40,
+        "residualCapacity": 40
+      },
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [
+          [
+            [
+              11.354767,
+              44.499027
+            ],
+            [
+              11.354703,
+              44.498874
+            ],
+            [
+              11.355067,
+              44.498767
+            ],
+            [
+              11.355148,
+              44.498939
+            ],
+            [
+              11.354767,
+              44.499027
+            ]
+          ]
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "id": 1003,
+        "name": "Filippo Re - Dip. Farmacia",
+        "maxCapacity": 10,
+        "residualCapacity": 10
+      },
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [
+          [
+            [
+              11.355025,
+              44.49936
+            ],
+            [
+              11.354982,
+              44.499276
+            ],
+            [
+              11.355169,
+              44.49923
+            ],
+            [
+              11.355218,
+              44.499321
+            ],
+            [
+              11.355025,
+              44.49936
+            ]
+          ]
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "id": 1004,
+        "name": "Irnerio - Dip. Fisica",
+        "maxCapacity": 20,
+        "residualCapacity": 20
+      },
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [
+          [
+            [
+              11.353539,
+              44.499329
+            ],
+            [
+              11.353485,
+              44.499237
+            ],
+            [
+              11.353791,
+              44.499161
+            ],
+            [
+              11.353844,
+              44.499253
+            ],
+            [
+              11.353539,
+              44.499329
+            ]
+          ]
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "id": 1005,
+        "name": "Puntoni",
+        "maxCapacity": 10,
+        "residualCapacity": 10
+      },
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [
+          [
+            [
+              11.353233,
+              44.497603
+            ],
+            [
+              11.353201,
+              44.497519
+            ],
+            [
+              11.353335,
+              44.497389
+            ],
+            [
+              11.353469,
+              44.497473
+            ],
+            [
+              11.353372,
+              44.497523
+            ],
+            [
+              11.353421,
+              44.497588
+            ],
+            [
+              11.353233,
+              44.497603
+            ]
+          ]
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "id": 1006,
+        "name": "Scaravilli - Dip. Economia",
+        "maxCapacity": 20,
+        "residualCapacity": 20
+      },
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [
+          [
+            [
+              11.351999,
+              44.497072
+            ],
+            [
+              11.35209,
+              44.496938
+            ],
+            [
+              11.352471,
+              44.497068
+            ],
+            [
+              11.351999,
+              44.497072
+            ]
+          ]
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "id": 1007,
+        "name": "Berti Pichat",
+        "maxCapacity": 60,
+        "residualCapacity": 60
+      },
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [
+          [
+            [
+              11.357251,
+              44.501108
+            ],
+            [
+              11.356773,
+              44.500871
+            ],
+            [
+              11.356768,
+              44.500779
+            ],
+            [
+              11.357208,
+              44.50076
+            ],
+            [
+              11.357251,
+              44.501108
+            ]
+          ]
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "id": 1008,
+        "name": "Mura Anteo Zamboni - Dip. Informatica",
+        "maxCapacity": 20,
+        "residualCapacity": 20
+      },
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [
+          [
+            [
+              11.356264,
+              44.497366
+            ],
+            [
+              11.356285,
+              44.497091
+            ],
+            [
+              11.356393,
+              44.497098
+            ],
+            [
+              11.356376,
+              44.49737
+            ],
+            [
+              11.356264,
+              44.497366
+            ]
+          ]
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "id": 1009,
+        "name": "San Giacomo",
+        "maxCapacity": 5,
+        "residualCapacity": 5
+      },
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [
+          [
+            [
+              11.356307,
+              44.496371
+            ],
+            [
+              11.35614,
+              44.496291
+            ],
+            [
+              11.356258,
+              44.496257
+            ],
+            [
+              11.356328,
+              44.496287
+            ],
+            [
+              11.356307,
+              44.496371
+            ]
+          ]
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "id": 1010,
+        "name": "Mura Anteo Zamboni - Dip. Biochimica",
+        "maxCapacity": 20,
+        "residualCapacity": 20
+      },
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [
+          [
+            [
+              11.355996,
+              44.496861
+            ],
+            [
+              11.35628,
+              44.496773
+            ],
+            [
+              11.356269,
+              44.496869
+            ],
+            [
+              11.355996,
+              44.496861
+            ]
+          ]
+        ]
+      }
+    }
+  ]
+};
 
   private map: Map | undefined;
 
