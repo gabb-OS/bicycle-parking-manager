9f19d7402b0b7392b7054342c5f318c3e40b8f3c
Gabriele Aprile <gabriele.aprile2@studio.unibo.it>
2026-01-12 18:17:05 +0100

    feature: added popup menu entry (three dots) for Login/Register fragment

--- app-bpm/app/src/main/java/com/contextaware/app_bpm/MainActivity.kt
+++ app-bpm/app/src/main/java/com/contextaware/app_bpm/MainActivity.kt
@@ -2,6 +2,7 @@ package com.contextaware.app_bpm
 
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
@@ -49,6 +50,17 @@ class MainActivity : AppCompatActivity() {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.main, menu)
         return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_login -> {
+                val navController = findNavController(R.id.nav_host_fragment_content_main)
+                navController.navigate(R.id.nav_login)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
