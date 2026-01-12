db73cbaca18c150ef2cc9c32bc2447d770d7c3ee
Gabriele Aprile <gabriele.aprile2@studio.unibo.it>
2026-01-12 18:07:51 +0100

    chore: added dependencies for Google Firebase authentication system

--- app-bpm/app/build.gradle.kts
+++ app-bpm/app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.google.gms.google.services)
 }
 
 android {
@@ -52,6 +53,8 @@ dependencies {
     implementation(libs.retrofit)
     implementation(libs.converter.gson)
     implementation(libs.play.services.location)
+    implementation(platform("com.google.firebase:firebase-bom:33.7.0"))
+    implementation(libs.firebase.auth)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
